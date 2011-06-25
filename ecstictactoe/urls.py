597a43addf570185ef8479feebb3f3f9ef9832a0
eric <eric@debian.hsd1.md.comcast.net>
2011-06-25 16:55:19 -0400

    add static directory, changed view names.

--- ecstictactoe/urls.py
+++ ecstictactoe/urls.py
@@ -3,15 +3,16 @@
 # 2011 Eric Schug
 # 
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 
 urlpatterns = patterns('',
     # board
-    (r'^$', 'ecstictactoe.tictactoe.views.game'),
+    (r'^$', 'ecstictactoe.tictactoe.views.gameView'),
     # move (ajax)
-    (r'^move/$', 'ecstictactoe.tictactoe.views.move'),
+    (r'^move/$', 'ecstictactoe.tictactoe.views.getmove'),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
 )
