7ccef77dff5150c6a1676c5a3a24127ac9aa5b38
eric <eric@debian.hsd1.md.comcast.net>
2011-06-25 23:06:30 -0400

    add some style

--- ecstictactoe/static/tictactoe.js
+++ ecstictactoe/static/tictactoe.js
@@ -2,8 +2,23 @@
 ** javascript
 ** 2011 Eric Schug
 */
-body {
-    /* green screen */
-    background-color: black;
-    color: green;
-}
+var left=0;
+var top=-20;
+var velup=0;
+
+function move()
+{
+    art=document.getElementById('art')
+    if(art) {
+        art.style.display='block'
+        art.style.top= (top-art.offsetHeight)+'px'
+        top += 5;
+        //console.log(art.offsetHeight);
+    }
+    if(top<445) {
+        t=setTimeout("move()",30);
+    }
+} 
+
+setTimeout('move()',10);
+
